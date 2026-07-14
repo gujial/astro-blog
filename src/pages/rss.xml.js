a9817bfbe39dd7bfee5973d965ce13ab5ff5c266
gujial <gu18647403665@outlook.com>
2026-07-14 16:16:32 +0800

    RSS 全文支持

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,15 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import { getImage } from "astro:assets";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+
+function escapeXmlAttr(value) {
+	return value
+		.replaceAll("&", "&amp;")
+		.replaceAll('"', "&quot;")
+		.replaceAll("<", "&lt;")
+		.replaceAll(">", "&gt;");
+}
 
 export async function GET(context) {
 	const posts = await getCollection("blog");
@@ -10,36 +19,42 @@ export async function GET(context) {
 		.map((post) => post.data.updatedDate ?? post.data.pubDate)
 		.sort((a, b) => b.getTime() - a.getTime())[0];
 
-	const items = posts
-		.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime())
-		.map((post) => {
-			const postUrl = new URL(`/blog/${post.id}/`, context.site).href;
-			const heroImageUrl = post.data.heroImage
-				? new URL(post.data.heroImage.src, context.site).href
-				: undefined;
+	const items = await Promise.all(
+		posts
+			.sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime())
+			.map(async (post) => {
+				const postUrl = new URL(`/blog/${post.id}/`, context.site).href;
+				const fullHtml = post.rendered?.html ?? `<p>${post.data.description}</p>`;
 
-			const mediaTags = heroImageUrl
-				? [
-					`<media:content url="${heroImageUrl}" medium="image" type="image/${post.data.heroImage.format}" width="${post.data.heroImage.width}" height="${post.data.heroImage.height}" />`,
-					`<media:thumbnail url="${heroImageUrl}" width="${post.data.heroImage.width}" height="${post.data.heroImage.height}" />`,
-				  ].join("")
-				: "";
+				let heroImageUrl;
+				if (post.data.heroImage) {
+					const optimized = await getImage({ src: post.data.heroImage });
+					heroImageUrl = new URL(optimized.src, context.site).href;
+				}
 
-			const contentHtml = heroImageUrl
-				? `<p><img src="${heroImageUrl}" alt="${post.data.title}" /></p><p>${post.data.description}</p>`
-				: `<p>${post.data.description}</p>`;
+				const mediaTags = heroImageUrl
+					? [
+						`<media:content url="${heroImageUrl}" medium="image" type="image/${post.data.heroImage.format}" width="${post.data.heroImage.width}" height="${post.data.heroImage.height}" />`,
+						`<media:thumbnail url="${heroImageUrl}" width="${post.data.heroImage.width}" height="${post.data.heroImage.height}" />`,
+					  ].join("")
+					: "";
 
-			return {
-				title: post.data.title,
-				description: post.data.description,
-				pubDate: post.data.pubDate,
-				link: `/blog/${post.id}/`,
-				categories: post.data.tags,
-				author: "gujial@gujial.cc (gujial)",
-				content: contentHtml,
-				customData: `${mediaTags}<guid isPermaLink="true">${postUrl}</guid>`,
-			};
-		});
+				const coverHtml = heroImageUrl
+					? `<p><img src="${heroImageUrl}" alt="${escapeXmlAttr(post.data.title)}" /></p>`
+					: "";
+
+				return {
+					title: post.data.title,
+					description: post.data.description,
+					pubDate: post.data.pubDate,
+					link: `/blog/${post.id}/`,
+					categories: post.data.tags,
+					author: "gujial@gujial.cc (gujial)",
+					content: `${coverHtml}${fullHtml}`,
+					customData: `${mediaTags}<guid isPermaLink="true">${postUrl}</guid>`,
+				};
+			})
+	);
 
 	return rss({
 		title: SITE_TITLE,
